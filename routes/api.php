6092a5f8504c4a8421e26cf8fa31ee0a6530b55f
Daniel Assani <devasddaniel@gmail.com>
2026-05-25 10:53:59 +0200

    add all

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,15 @@ use App\Http\Controllers\Api\InvoiceGeneratorController;
 */
 Route::get('/generate-invoice', [InvoiceGeneratorController::class, 'test']);
 Route::post('/generate-invoice', [InvoiceGeneratorController::class, 'test']);
+Route::post('/generate-quotation', [InvoiceGeneratorController::class, 'test']);
+Route::post('/generate-delivery', [InvoiceGeneratorController::class, 'test']);
+Route::post('/linedelivery', [InvoiceGeneratorController::class, 'test']);
+Route::post('/delivery-line', [InvoiceGeneratorController::class, 'test']);
+Route::post('/deliveryline', [InvoiceGeneratorController::class, 'test']);
+Route::post('/lineinvoice', [InvoiceGeneratorController::class, 'test']);
+Route::post('/linequotation', [InvoiceGeneratorController::class, 'test']);
+Route::post('/linecotation', [InvoiceGeneratorController::class, 'test']);
+
+
+
 //Route::post('/generate-invoice', [InvoiceGeneratorController::class, 'generate']);
